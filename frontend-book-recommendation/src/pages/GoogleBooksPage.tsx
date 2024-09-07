de494d2ad682b487162779f5c68cded74e972957
celobastos <marceloviniciusbs17@gmail.com>
2024-09-07 01:43:01 -0300

    Feat: Card com detalhamento do google Search

--- frontend-book-recommendation/src/pages/GoogleBooksPage.tsx
+++ frontend-book-recommendation/src/pages/GoogleBooksPage.tsx
@@ -10,19 +10,40 @@ const GoogleBooksPage: React.FC = () => {
     setSearchResults(results);
   };
 
+  // Function to render star rating
+  const renderStars = (rating: number) => {
+    const totalStars = 5;
+    return (
+      <span className={styles.stars}>
+        {[...Array(totalStars)].map((_, i) => (
+          <i
+            key={i}
+            className={`fa fa-star ${i < rating ? styles.filledStar : styles.emptyStar}`}
+            aria-hidden="true"
+          />
+        ))}
+      </span>
+    );
+  };
+
   return (
     <div className={styles.container}>
-         <NavBar />
-      <h1>Google Books Search</h1>
+      <NavBar />
+      <h1 className={styles.title}>Google Books Search</h1>
       <GoogleSearchBar onResults={handleSearchResults} />
 
       {searchResults && (
         <div className={styles.results}>
-          <h2>Search Results:</h2>
           <div className={styles.resultCard}>
-            <h3>{searchResults.title}</h3>
-            <p>{searchResults.author}</p>
-            <img src={searchResults.image} alt={searchResults.title} />
+            <img src={searchResults.image} alt={searchResults.title} className={styles.bookImage} />
+            <div className={styles.bookDetails}>
+              <h3 className={styles.bookTitle}>{searchResults.title}</h3>
+              <div className={styles.authorRating}>
+                <p className={styles.bookAuthor}><strong>Author:</strong> {searchResults.author}</p>
+                {searchResults.averageRating && renderStars(searchResults.averageRating)}
+              </div>
+              <p className={styles.bookDescription}><strong>Description:</strong> {searchResults.description}</p>
+            </div>
           </div>
         </div>
       )}
