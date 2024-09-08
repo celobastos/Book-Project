aa7336cd1d7046cc0ed7f38bef51197d77c6fae0
celobastos <marceloviniciusbs17@gmail.com>
2024-09-08 17:32:23 -0300

    Style: Ajustes de responsividade nas paginas

--- frontend-book-recommendation/src/components/Functionalities/FunctionalitiesSection.tsx
+++ frontend-book-recommendation/src/components/Functionalities/FunctionalitiesSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './FunctionalitiesSection.module.css'; // Import the styles
+import styles from './FunctionalitiesSection.module.css'; 
 
 const FunctionalitiesSection: React.FC = () => {
   return (
@@ -8,7 +8,7 @@ const FunctionalitiesSection: React.FC = () => {
       <div className={styles.gridContainer}>
         <div className={styles.gridItem}>
           <div className={styles.iconContainer}>
-            <i className="fas fa-book"></i> {/* Example icon */}
+            <i className="fas fa-book"></i> 
           </div>
           <h3>Adicione e salve um livro que você leu</h3>
           <p>Adicione e mantenha um registro de todos os livros que você leu com facilidade.</p>
@@ -16,7 +16,7 @@ const FunctionalitiesSection: React.FC = () => {
 
         <div className={styles.gridItem}>
           <div className={styles.iconContainer}>
-            <i className="fas fa-info-circle"></i> {/* Example icon */}
+            <i className="fas fa-info-circle"></i> 
           </div>
           <h3>Veja os detalhes sobre os seus livros</h3>
           <p>Veja informações detalhadas sobre seus livros, como título, autor e data de leitura.</p>
@@ -24,7 +24,7 @@ const FunctionalitiesSection: React.FC = () => {
 
         <div className={styles.gridItem}>
           <div className={styles.iconContainer}>
-            <i className="fas fa-search"></i> {/* Example icon */}
+            <i className="fas fa-search"></i> 
           </div>
           <h3>Pesquise as avaliações do Google de um livro</h3>
           <p>Pesquise e veja as avaliações do Google Books para descobrir a opinião de outros leitores.</p>
@@ -32,7 +32,7 @@ const FunctionalitiesSection: React.FC = () => {
 
         <div className={styles.gridItem}>
           <div className={styles.iconContainer}>
-            <i className="fas fa-archive"></i> {/* Example icon */}
+            <i className="fas fa-archive"></i> 
           </div>
           <h3>Veja o repositório de seus livros</h3>
           <p>Acesse rapidamente o repositório de todos os livros que você leu ao longo do tempo.</p>
